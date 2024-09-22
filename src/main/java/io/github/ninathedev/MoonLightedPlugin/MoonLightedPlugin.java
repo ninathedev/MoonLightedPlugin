d62ced04240ea5f961990d55ca0b929ec21bd7fb
ninathedev <145730763+ninathedev@users.noreply.github.com>
2024-09-22 14:43:33 +0800

    lessgoo

---
+++ src/main/java/io/github/ninathedev/MoonLightedPlugin/MoonLightedPlugin.java
@@ -0,0 +1,17 @@
+package io.github.ninathedev.MoonLightedPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoonLightedPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
